e1aa2e97e5287606f268a3f37302c50b4eed11d6
OraClassic <85005984+OraClassic@users.noreply.github.com>
2025-06-16 14:44:45 +0900

    Update clipboard.js

--- assets/js/clipboard.js
+++ assets/js/clipboard.js
@@ -1,24 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll('pre > code').forEach((codeBlock) => {
+  document.querySelectorAll('.highlight pre').forEach((pre) => {
     const button = document.createElement("button");
     button.className = "copy-button";
     button.type = "button";
     button.innerText = "Copy";
 
     button.addEventListener("click", () => {
-      navigator.clipboard.writeText(codeBlock.innerText).then(() => {
+      const code = pre.innerText;
+      navigator.clipboard.writeText(code).then(() => {
         button.innerText = "Copied!";
         setTimeout(() => {
           button.innerText = "Copy";
         }, 2000);
+      }).catch((err) => {
+        console.error('복사 실패:', err);
+        button.innerText = "Error";
       });
     });
 
-    const pre = codeBlock.parentNode;
-    if (pre.parentNode.classList.contains("highlight")) {
-      pre.parentNode.insertBefore(button, pre);
-    } else {
-      pre.insertBefore(button, codeBlock);
-    }
+    pre.parentNode.style.position = "relative";
+    pre.parentNode.appendChild(button);
   });
 });
